2ef686b643953c0231e811bf36f181f084524295
Ari <arikaputkin@gmail.com>
2018-02-27 15:27:11 -0500

    2/27/18

--- js/script.js
+++ js/script.js
@@ -23,13 +23,19 @@ var Accommodation10_15 = L.tileLayer('data/Accommodation10_15/data/{z}/{x}/{y}.p
   maxZoom: 15
 })
 
+var Total10_15 = L.tileLayer('data/Total10_15/Total10h_15/data/{z}/{x}/{y}.png', {
+                  maxZoom: 15
+                    })
+                L.tileLayer('data/Total10_15/Total10w_15//data/{z}/{x}/{y}.png', {
+                  maxZoom: 15})
 
 
 var Sectorslayer = {
   "Finance 2005": finance10_05,
   "Finance 2015": finance10_15,
   "Accommodation 2005": Accommodation10_05,
-  "Accommodation 2015": Accommodation10_15
+  "Accommodation 2015": Accommodation10_15,
+  "Total 2015": Total10_15
 };
 
 L.control.layers(Sectorslayer,null,{collapsed:false, position: 'topright'}).addTo(map);
